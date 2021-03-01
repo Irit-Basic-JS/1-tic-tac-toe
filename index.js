4f6980620c61247c5490a5c6d76e5e8bcca8777b
gisinka <mrxenonmr@gmail.com>
2021-03-01 21:58:56 +0500

    task done

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+let matrix = createMatrix(3);
+let isOver = false;
+let currentPlayer = CROSS;
+
 startGame();
 addResetListener();
 
 function startGame () {
+    matrix = createMatrix(3);
+    isOver = false;
+    currentPlayer = CROSS;
     renderGrid(3);
 }
 
@@ -27,7 +34,24 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
+    isOver ||= isMatrixFilled();
+    if (isOver || matrix[row][col] !== EMPTY) { return; }
+    matrix[row][col] = currentPlayer;
+    renderSymbolInCell(currentPlayer, row, col);
+    let coords = getWinCoords();
+    if (coords.length != 0) {
+        for (let point of coords) {
+            renderSymbolInCell(currentPlayer, point[0], point[1], '#ff0000');
+        }
+        isOver = true;
+        alert(`${currentPlayer} is win!`);
+        return;
+    }
+    if (isMatrixFilled()) {
+        isOver = true;
+        alert("Победила дружба");
+    }
+    currentPlayer = currentPlayer === CROSS ? ZERO : CROSS;
     console.log(`Clicked on cell: ${row}, ${col}`);
 
 
@@ -36,9 +60,42 @@ function cellClickHandler (row, col) {
      */
 }
 
+function getWinCoords() {
+    let symbol = matrix[0][0];
+    if (symbol === matrix[0][1] && symbol == matrix[0][2] && symbol != EMPTY) {return [[0, 0], [0, 1], [0, 2]];}
+    if (symbol === matrix[1][0] && symbol == matrix[2][0] && symbol != EMPTY) {return [[0, 0], [1, 0], [2, 0]];}
+    symbol = matrix[2][2];
+    if (symbol === matrix[2][1] && symbol == matrix[2][0] && symbol != EMPTY) {return [[2, 0], [2, 1], [2, 2]];}
+    if (symbol === matrix[0][2] && symbol == matrix[1][2] && symbol != EMPTY) {return [[0, 2], [1, 2], [2, 2]];}
+    symbol = matrix[1][1];
+    if (symbol === matrix[0][0] && symbol == matrix[2][2] && symbol != EMPTY) {return [[0, 0], [1, 1], [2, 2]];}
+    if (symbol === matrix[2][0] && symbol == matrix[0][2] && symbol != EMPTY) {return [[2, 0], [1, 1], [0, 2]];}
+    if (symbol === matrix[1][0] && symbol == matrix[1][2] && symbol != EMPTY) {return [[1, 0], [1, 1], [1, 2]];}
+    if (symbol === matrix[0][1] && symbol == matrix[2][1] && symbol != EMPTY) {return [[0, 1], [1, 1], [2, 1]];}
+    return [];
+}
+
+function isMatrixFilled() {
+    for (let row of matrix) {
+        if (row.some(i => i === EMPTY))
+            return false;
+    }
+    return true;
+}
+
+function createMatrix(dimension) {
+    let field = [];
+    for (let i = 0; i < dimension; i++) {
+        field[i] = [];
+        for (let j = 0; j < dimension; j++) {
+            field[i][j] = EMPTY;
+        }
+    }
+    return field;
+}
+
 function renderSymbolInCell (symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
-
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
@@ -54,6 +111,7 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
+    startGame();
     console.log('reset!');
 }
 
