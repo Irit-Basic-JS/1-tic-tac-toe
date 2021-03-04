db97a48e003c22e27db76ff96d5216ba6bd60178
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2021-03-04 21:10:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,149 +1,198 @@
-const CROSS = 'X';
-const ZERO = 'O';
-const EMPTY = ' ';
-const names = {'X': "Navalny", 'O': "Putin"}
+const CROSS = "X";
+const ZERO = "O";
+const EMPTY = " ";
+const names = {"X": "Navalny", "O": "Putin"};
+const defaultDimension = 3;
+let dimension = 3;
 
-const container = document.getElementById('fieldWrapper');
+const container = document.getElementById("fieldWrapper");
 
 let player = CROSS;
 let isOver = false;
+let field;
 startGame();
 addResetListener();
-let field = createField(3);
 
 function startGame() {
-    renderGrid(3);
+	const dim = prompt("Enter field dimension");
+	if (dim) {
+		dimension = dim;
+	} else {
+		dimension = defaultDimension;
+	}
+	player = CROSS;
+	renderGrid(dimension, () => EMPTY);
+	field = createField(dimension);
+	isOver = false;
 }
 
-function renderGrid(dimension) {
-    container.innerHTML = '';
-
-    for (let i = 0; i < dimension; i++) {
-        const row = document.createElement('tr');
-        for (let j = 0; j < dimension; j++) {
-            const cell = document.createElement('td');
-            cell.textContent = EMPTY;
-            cell.addEventListener('click', () => cellClickHandler(i, j));
-            row.appendChild(cell);
-        }
-        container.appendChild(row);
-    }
+function renderGrid(dimension, getCellValue) {
+	container.innerHTML = "";
+	
+	for (let i = 0; i < dimension; i++) {
+		const row = document.createElement("tr");
+		for (let j = 0; j < dimension; j++) {
+			const cell = document.createElement("td");
+			cell.textContent = getCellValue(i, j);
+			cell.addEventListener("click", () => cellClickHandler(i, j));
+			row.appendChild(cell);
+		}
+		container.appendChild(row);
+	}
 }
 
 function cellClickHandler(row, col) {
-    isOver ||= isAllFieldFill();
-    if (isOver || field[row][col] !== EMPTY)
-        return;
-    field[row][col] = player;
-    renderSymbolInCell(player, row, col);
-    let coordinates = getWinCoordinates();
-    if (coordinates.length !== 0) {
-        recolorWinCells(coordinates);
-        isOver = true;
-        alert(`${names[player]} is win!`);
-        return;
-    }
-    if (isAllFieldFill()) {
-        isOver = true;
-        alert("Победила дружба");
-    }
-    changePlayer();
+	let emptyCells = getEmptyCells();
+	isOver ||= emptyCells.length === 0;
+	if (isOver || field[row][col] !== EMPTY)
+		return;
+	field[row][col] = player;
+	emptyCells = emptyCells.filter(cell => cell.x !== col || cell.y !== row);
+	renderSymbolInCell(player, row, col);
+	const lines = getLinesAndDiagonals();
+	let coordinates = getWinCoordinates(lines);
+	if (coordinates.length !== 0) {
+		recolorWinCells(coordinates);
+		isOver = true;
+		alert(`${names[player]} is win!`);
+		return;
+	}
+	if (emptyCells.length === 0) {
+		isOver = true;
+		alert("Победила дружба");
+	}
+	changePlayer();
+	if (player === ZERO) {
+		const cells = lines.find(line => line.some(x => x.value === EMPTY) && line.every(x => x.value !== CROSS));
+		const cell = cells ? cells.find(x => x.value === EMPTY) : emptyCells[getRandomInt(emptyCells.length - 1)];
+		cellClickHandler(cell.y, cell.x);
+		return;
+	}
+	if (emptyCells.length <= dimension * dimension / 2) {
+		addDimension();
+	}
 }
 
-function renderSymbolInCell(symbol, row, col, color = '#333') {
-    const targetCell = findCell(row, col);
-    targetCell.textContent = symbol;
-    targetCell.style.color = color;
+
+function renderSymbolInCell(symbol, row, col, color = "#333") {
+	const targetCell = findCell(row, col);
+	targetCell.textContent = symbol;
+	targetCell.style.color = color;
 }
 
 function findCell(row, col) {
-    const targetRow = container.querySelectorAll('tr')[row];
-    return targetRow.querySelectorAll('td')[col];
+	const targetRow = container.querySelectorAll("tr")[row];
+	return targetRow.querySelectorAll("td")[col];
 }
 
 function addResetListener() {
-    const resetButton = document.getElementById('reset');
-    resetButton.addEventListener('click', resetClickHandler);
+	const resetButton = document.getElementById("reset");
+	resetButton.addEventListener("click", startGame);
 }
 
-function resetClickHandler() {
-    field = createField(3);
-    renderGrid(3);
-    isOver = false;
-}
-
-function recolorWinCells(coordinates, color = '#ff0000') {
-    for (let coordinate of coordinates) {
-        renderSymbolInCell(player, coordinate.y, coordinate.x, color)
-    }
+function recolorWinCells(coordinates, color = "#ff0000") {
+	for (let coordinate of coordinates) {
+		renderSymbolInCell(coordinate.value, coordinate.y, coordinate.x, color);
+	}
 }
 
 function createField(dimension) {
-    let result = [];
-    for (let i = 0; i < dimension; i++) {
-        result[i] = [];
-        for (let j = 0; j < dimension; j++) {
-            result[i][j] = EMPTY;
-        }
-    }
-    return result;
+	let result = [];
+	for (let i = 0; i < dimension; i++) {
+		result[i] = [];
+		for (let j = 0; j < dimension; j++) {
+			result[i][j] = EMPTY;
+		}
+	}
+	return result;
 }
 
 function changePlayer() {
-    player = player === CROSS ? ZERO : CROSS;
+	player = player === CROSS ? ZERO : CROSS;
 }
 
-function isAllFieldFill() {
-    for (let row of field) {
-        if (row.some(x => x === EMPTY))
-            return false;
-    }
-    return true;
-}
-
-function getWinCoordinates() {
-    for (let i = 0; i < field.length; i++) {
-        if (isWinSet(field[i])) {
-            return field[i].map((value, index) => {
-                return {x: index, y: i};
-            })
-        }
-    }
-
-    for (let i = 0; i < field.length; i++) {
-        let column = field.map(value => value[i]);
-        if (isWinSet(column))
-            return field.map((value, index) => {
-                return {x: i, y: index};
-            })
-    }
-
-    let first = [];
-    for (let i = 0; i < field.length; i++) {
-        first[i] = field[i][i];
-    }
-    if (isWinSet(first)) {
-        return field.map((value, index) => {
-            return {x: index, y: index};
-        });
-    }
-    let second = [];
-    for (let i = 0; i < field.length; i++) {
-        second[i] = field[field.length - i - 1][i];
-    }
-
-    if (isWinSet(second)) {
-        return field.map((value, index) => {
-            return {x: field.length - index - 1, y: index};
-        })
-    }
-
-    return [];
+function getWinCoordinates(lines) {
+	for (const line of lines) {
+		if (isWinSet(line))
+			return line;
+	}
+	return [];
 }
 
 function isWinSet(arr) {
-    let set = new Set(arr)
-    return set.size === 1 && !set.has(EMPTY);
+	return new Set(arr.map(x => x.value)).size === 1 && arr[0].value !== EMPTY;
+}
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getEmptyCells() {
+	let result = [];
+	for (let i = 0; i < dimension; i++) {
+		for (let j = 0; j < dimension; j++) {
+			if (field[i][j] === EMPTY)
+				result.push({x: j, y: i, value: EMPTY});
+		}
+	}
+	return result;
+}
+
+function addDimension() {
+	dimension++;
+	for (let row of field) {
+		row.push(EMPTY);
+	}
+	let arr = new Array(dimension);
+	field.push(arr);
+	for (let i = 0; i < dimension; i++) {
+		arr[i] = EMPTY;
+	}
+	renderGrid(dimension, (row, column) => field[row][column]);
 }
 
 
+function getLinesAndDiagonals() {
+	let result = [];
+	result = result.concat(getHorizontalLines());
+	result = result.concat(getVerticalLines());
+	result.push(getLeftSlash());
+	result.push(getRightSlash());
+	return result;
+}
+
+function getHorizontalLines() {
+	let result = [];
+	for (let i = 0; i < field.length; i++) {
+		result.push(field[i].map((value, index) => {
+			return {x: index, y: i, value: value};
+		}));
+	}
+	return result;
+}
+
+function getVerticalLines() {
+	let result = [];
+	for (let i = 0; i < field.length; i++) {
+		result.push(field.map((value, index) => {
+			return {x: i, y: index, value: field[index][i]};
+		}));
+	}
+	return result;
+}
+
+function getLeftSlash() {
+	let result = [];
+	for (let i = 0; i < field.length; i++) {
+		result.push({x: i, y: i, value: field[i][i]});
+	}
+	return result;
+}
+
+function getRightSlash() {
+	let result = [];
+	for (let i = 0; i < field.length; i++) {
+		result.push({x: i, y: field.length - i - 1, value: field[field.length - i - 1][i]});
+	}
+	return result;
+}
