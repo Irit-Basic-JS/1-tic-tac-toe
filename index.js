0f2651bb32839f4a6e4f754f23983460bf3ca09c
Aleksander Zverev <aleksander.zverev.lt@gmail.com>
2021-03-06 15:14:54 +0500

    0,10,11 works well

--- index.js
+++ index.js
@@ -5,19 +5,61 @@ const RED_COLOR = '#ff0000';
 
 const container = document.getElementById('fieldWrapper');
 
+let fieldRows, fieldColumns;
+let field, hasWinner, winner, stepCount, maxSteps;
+let isUsingAI, isSimpleAI;
+
 startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    let rows = getNumber("Input rows number", "3");
+    let columns = getNumber("Input columns number", "3");
+    isUsingAI = confirm("Use AI?");
+    if (isUsingAI)
+        isSimpleAI = confirm("Use simple AI?");
+
+    InitializeDependencies(rows, columns);
+
+    renderGrid(fieldRows, fieldColumns);
 }
 
-function renderGrid (dimension) {
+function InitializeDependencies(rows, columns)
+{
+    fieldRows = rows;
+    fieldColumns = columns;
+    field = createField(fieldRows, fieldColumns);
+    hasWinner = false;
+    winner = undefined;
+    stepCount = 0;
+    maxSteps = fieldRows * fieldColumns;
+}
+
+function getNumber(text, defaultInput) {
+    while (true)
+    {
+        let number = Number(prompt(text, defaultInput));
+        if (!isNaN(number)) {
+            return number;
+        }
+        alert("Incorrect input!");
+    }
+}
+
+function createField(rows, columns) {
+    let array = [];
+    for (let i = 0; i < rows; i++) {
+        array.push(new Array(columns));
+    }
+    return array;
+}
+
+function renderGrid (rows, columns) {
     container.innerHTML = '';
 
-    for (let i = 0; i < dimension; i++) {
+    for (let i = 0; i < rows; i++) {
         const row = document.createElement('tr');
-        for (let j = 0; j < dimension; j++) {
+        for (let j = 0; j < columns; j++) {
             const cell = document.createElement('td');
             cell.textContent = EMPTY;
             cell.addEventListener('click', () => cellClickHandler(i, j));
@@ -32,17 +74,6 @@ function addResetListener () {
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-
-let field = [
-    new Array(3),
-    new Array(3),
-    new Array(3)
-];
-
-let hasWinner = false;
-let winner = undefined;
-let stepCount = 0;
-
 function cellClickHandler (row, col) {
     console.log(`Clicked on cell: ${row}, ${col}`);
 
@@ -50,6 +81,7 @@ function cellClickHandler (row, col) {
         return;
 
     let currentSymbol = stepCount % 2 === 0 ? CROSS : ZERO;
+
     let isSymbolSet = setSymbolOnField(currentSymbol, row, col);
 
     if (isSymbolSet) {
@@ -59,24 +91,87 @@ function cellClickHandler (row, col) {
     checkWinner();
     stepCount++;
 
-    if (stepCount >= 9 && !hasWinner)
+    if (stepCount >= maxSteps && !hasWinner)
     {
         alert("Победила дружба!");
         hasWinner = true;
     }
+
+    if (!hasWinner && isUsingAI && currentSymbol === CROSS)
+    {
+        isSimpleAI
+            ? playSimpleAI(ZERO)
+            : playDifficultAI(ZERO);
+    }
+}
+
+function playDifficultAI(symbol) {
+    for (let row = 0; row < fieldRows; row++) {
+        for (let col = 0; col < fieldColumns; col++) {
+            let isSet = setSymbolOnField(symbol, row, col);
+            if (isSet && containsWinner()) {
+                unsetSymbolOnField(row, col);
+                cellClickHandler(row,col);
+                return;
+            }
+
+            if (isSet) {
+                unsetSymbolOnField(row, col);
+            }
+        }
+    }
+
+    playSimpleAI(symbol);
+}
+
+function playSimpleAI(symbol) {
+    while (true) {
+        let row = randomInteger(0, fieldRows - 1);
+        let col = randomInteger(0, fieldColumns - 1);
+        if (setSymbolOnField(symbol, row, col)) {
+            unsetSymbolOnField(row,col);
+            cellClickHandler(row,col);
+            return;
+        }
+    }
+}
+
+function randomInteger(min, max) {
+    let num = min + Math.random() * (max + 1 - min);
+    return Math.floor(num);
+}
+
+function containsWinner() {
+    return hasHorizontalWinner().hasWinner
+        || hasVerticalWinner().hasWinner
+        || hasLeftSlashWinner()
+        || hasRightSlashWinner();
 }
 
 function setSymbolOnField(symbol, row, col)
 {
-    if (field[row][col] === undefined){
+    if (isSellEmpty(row, col)){
         field[row][col] = symbol;
         return true;
     }
     return false;
 }
 
+function unsetSymbolOnField(row, col) {
+    if (!isSellEmpty(row, col)) {
+        field[row][col] = undefined;
+        return true;
+    }
+    return false;
+}
+
+function isSellEmpty(row, col) {
+    return field[row][col] === undefined;
+}
+
 function checkWinner(){
-    checkHorizontalAndVertical();
+    checkHorizontal();
+    checkVertical();
     checkLeftSlash();
     checkRightSlash();
 
@@ -86,99 +181,157 @@ function checkWinner(){
     }
 }
 
-function checkHorizontalAndVertical() {
-
-    for (let row = 0; row < 3; row++) {
-        let countCrossHor = 0, countCrossVer = 0;
-        let countZeroHor = 0, countZeroVer = 0;
-
-        for (let col = 0; col < 3; col++) {
-            if (field[row][col] === ZERO)
-                countZeroHor++;
-            else if (field[row][col] === CROSS)
-                countCrossHor++;
-
-            if (field[col][row] === ZERO)
-                countZeroVer++;
-            else if (field[col][row] === CROSS)
-                countCrossVer++;
-        }
-
-        if (countCrossHor === 3 || countZeroHor === 3) {
-            hasWinner = true;
-            winner = field[row][0];
-            markHorizontalWinner(row);
-            break;
-        }
-
-        if (countCrossVer === 3 || countZeroVer === 3) {
-            hasWinner = true;
-            winner = field[0][row];
-            markVerticalWinner(row);
-            break;
-        }
+function checkHorizontal() {
+    let result = hasHorizontalWinner();
+    if (result.hasWinner) {
+        hasWinner = true;
+        winner = field[result.row][result.column];
+        markHorizontalWinner(result.row);
     }
 }
 
+function checkVertical() {
+    let result = hasVerticalWinner();
+    if (result.hasWinner){
+        hasWinner = true;
+        winner = field[result.row][result.column];
+        markVerticalWinner(result.column);
+    }
+
+    return result;
+}
+
+function checkLeftSlash() {
+    if (hasLeftSlashWinner()) {
+        hasWinner = true;
+        winner = field[0][0];
+        markLeftSlash();
+    }
+}
+
+function checkRightSlash() {
+    if (hasRightSlashWinner()) {
+        hasWinner = true;
+        winner = field[0][fieldColumns - 1];
+        markRightSlash();
+    }
+}
+
+function hasHorizontalWinner(){
+    let result = {
+        hasWinner: false,
+        row: undefined,
+        column: undefined
+    }
+
+    for (let row = 0; row < fieldRows; row++) {
+        let countCross = 0, countZero = 0;
+
+        for (let col = 0; col < fieldColumns; col++) {
+            if (field[row][col] === ZERO)
+                countZero++;
+            else if (field[row][col] === CROSS)
+                countCross++;
+        }
+
+        if (countCross === fieldRows || countZero === fieldRows) {
+            result.hasWinner = true;
+            result.row = row;
+            result.column = 0;
+            break;
+        }
+    }
+
+    return result;
+}
+
+function hasVerticalWinner() {
+    let result = {
+        hasWinner: false,
+        row: undefined,
+        column: undefined
+    }
+
+    for (let col = 0; col < fieldColumns; col++) {
+        let countCross = 0, countZero = 0;
+
+        for (let row = 0; row < fieldRows; row++) {
+            if (field[row][col] === ZERO)
+                countZero++;
+            else if (field[row][col] === CROSS)
+                countCross++;
+        }
+
+        if (countCross === fieldColumns || countCross === fieldColumns) {
+            result.hasWinner = true;
+            result.row = 0;
+            result.column = col
+            break;
+        }
+    }
+    return result;
+}
+
+function hasLeftSlashWinner() {
+    if (fieldRows !== fieldColumns || field[0][0] === undefined) {
+        return false;
+    }
+
+    let sym = field[0][0];
+    for (let i = 0; i < fieldColumns; i++) {
+        if (field[i][i] !== sym) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function hasRightSlashWinner() {
+    if (fieldRows !== fieldColumns || field[0][fieldColumns - 1] === undefined) {
+        return false;
+    }
+
+    let sym = field[0][fieldColumns - 1];
+
+    for (let col = fieldColumns - 1, row = 0; col >= 0 && row < fieldRows; col--, row++) {
+        if (field[row][col] !== sym) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function markHorizontalWinner(row) {
-    let sym = field[row][0];
-    for (let i = 0; i < 3; i++){
-        renderSymbolInCell(sym, row, i, RED_COLOR);
+    for (let i = 0; i < fieldColumns; i++){
+        renderSymbolInCell(winner, row, i, RED_COLOR);
     }
 }
 
 function markVerticalWinner(col) {
-    let sym = field[0][col];
-    for (let i = 0; i < 3; i++){
-        renderSymbolInCell(sym, i, col, RED_COLOR);
+    for (let i = 0; i < fieldRows; i++){
+        renderSymbolInCell(winner, i, col, RED_COLOR);
     }
-}
-
-function checkLeftSlash() {
-    if (field[0][0] === undefined) {
-        return;
-    }
-
-    let sym = field[0][0];
-    for (let i = 0; i < 3; i++) {
-        if (field[i][i] !== sym) {
-            return;
-        }
-    }
-
-    hasWinner = true;
-    winner = sym;
-    markLeftSlash();
-}
-
-function checkRightSlash() {
-    if (field[0][2] === undefined) {
-        return;
-    }
-
-    let sym = field[0][2];
-    for (let i = 2, j = 0; i >= 0 && j < 3; i--, j++) {
-        if (field[j][i] !== sym) {
-            return;
-        }
-    }
-
-    hasWinner = true;
-    winner = sym;
-    markRightSlash();
 }
 
 function markLeftSlash() {
+    if (fieldRows !== fieldColumns) {
+        return;
+    }
+
     let sym = field[0][0];
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < fieldRows; i++) {
         renderSymbolInCell(sym, i, i, RED_COLOR);
     }
 }
 
 function markRightSlash() {
-    let sym = field[0][2];
-    for (let i = 2, j = 0; i >= 0 && j < 3; i--, j++) {
-        renderSymbolInCell(sym, j, i, RED_COLOR);
+    if (fieldRows !== fieldColumns) {
+        return;
+    }
+
+    let sym = field[0][fieldColumns - 1];
+    for (let col = fieldColumns - 1, row = 0; col >= 0 && row < fieldRows; col--, row++) {
+        renderSymbolInCell(sym, row, col, RED_COLOR);
     }
 }
 
@@ -197,19 +350,12 @@ function findCell (row, col) {
 function resetClickHandler () {
     console.log('reset!');
 
+    startGame()
+
     hasWinner = false;
     winner = undefined;
     stepCount = 0;
-
-    field = [
-        new Array(3),
-        new Array(3),
-        new Array(3)
-    ];
-
-    for (let i = 0; i < 3; i++)
-        for (let j = 0; j < 3; j++)
-            renderSymbolInCell(EMPTY, i, j);
+    maxSteps = fieldRows * fieldColumns;
 }
 
 
