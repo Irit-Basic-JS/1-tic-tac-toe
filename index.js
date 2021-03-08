5da7f254a9b4f6f4bd1b3e9cb5de24e31663d877
Denis <defimov9@gmail.com>
2021-03-09 03:13:56 +0500

    Task done

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
 
+
+let field = [];
+let dimension = 3;
+let countMoves = 9;
+let endGame = false;
+
 const container = document.getElementById('fieldWrapper');
 
 startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    renderGrid(dimension);
 }
 
 function renderGrid (dimension) {
     container.innerHTML = '';
-
+    createField(dimension);
     for (let i = 0; i < dimension; i++) {
         const row = document.createElement('tr');
         for (let j = 0; j < dimension; j++) {
@@ -26,14 +32,35 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
+function createField(dimension) {
+    for (let i = 0; i < dimension; i++) {
+        field[i] = new Array();
+        for (let j = 0; j < dimension; j++) {
+            field[i][j] = EMPTY;
+        }
+    }
+}
 
-
-    /* Пользоваться методом для размещения символа в клетке так:
+function moveCheck(row, col) {
+    if (countMoves % 2 === 0) {
+        field[row][col] = ZERO;
         renderSymbolInCell(ZERO, row, col);
-     */
+        countMoves--;
+    }
+    else {
+        field[row][col] = CROSS;
+        renderSymbolInCell(CROSS, row, col);
+        countMoves--;
+    }
+    checkWinner();
+}
+
+function cellClickHandler (row, col) {
+    
+    if (field[row][col] === EMPTY && !endGame) {
+        moveCheck(row, col);
+    }
+    console.log(`Clicked on cell: ${row}, ${col}`);
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -54,34 +81,108 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
+    countMoves = 9;
+    endGame = false;
+    startGame();
     console.log('reset!');
 }
 
-
-/* Test Function */
-/* Победа первого игрока */
-function testWin () {
-    clickOnCell(0, 2);
-    clickOnCell(0, 0);
-    clickOnCell(2, 0);
-    clickOnCell(1, 1);
-    clickOnCell(2, 2);
-    clickOnCell(1, 2);
-    clickOnCell(2, 1);
+function checkWinner() {
+    checkHorizontalWinner();
+    checkVerticalWinner();
+    checkDiagonalWinner();
+    if (countMoves === 0 && !endGame) {
+        endGame = true;
+        alert("Победила дружба!");
+    }
 }
 
-/* Ничья */
-function testDraw () {
-    clickOnCell(2, 0);
-    clickOnCell(1, 0);
-    clickOnCell(1, 1);
-    clickOnCell(0, 0);
-    clickOnCell(1, 2);
-    clickOnCell(1, 2);
-    clickOnCell(0, 2);
-    clickOnCell(0, 1);
-    clickOnCell(2, 1);
-    clickOnCell(2, 2);
+function checkHorizontalWinner() {
+    for (let i = 0; i < 3; i++) {
+        if (
+            field[i][0] === CROSS &&
+            field[i][1] === CROSS &&
+            field[i][2] === CROSS) {
+                endGame = true;
+                alert("Победили крестики!");
+                for (let j = 0; j < 3; j++) {
+                    let targetCell = findCell(i, j);
+                    targetCell.style.color = "red";
+                }
+            }
+        else if (
+            field[i][0] === ZERO &&
+            field[i][1] === ZERO &&
+            field[i][2] === ZERO) {
+                endGame = true;
+                alert("Победили нолики!");
+                for (let j = 0; j < 3; j++) {
+                    let targetCell = findCell(i, j);
+                    targetCell.style.color = "red";
+                }
+        }
+    }
+}
+
+function checkVerticalWinner() {
+    for (let i = 0; i < 3; i++) {
+        if (
+            field[0][i] === CROSS &&
+            field[1][i] === CROSS &&
+            field[2][i] === CROSS) {
+                endGame = true;
+                alert("Победили крестики!");
+                for (let j = 0; j < 3; j++) {
+                    let targetCell = findCell(j, i);
+                    targetCell.style.color = "red";
+                }
+            }
+        else if (
+            field[0][i] === ZERO &&
+            field[1][i] === ZERO &&
+            field[2][i] === ZERO) {
+                endGame = true;
+                alert("Победили нолики!");
+                for (let j = 0; j < 3; j++) {
+                    let targetCell = findCell(j, i);
+                    targetCell.style.color = "red";
+                }
+        }
+    }
+}
+
+function checkDiagonalWinner() {
+    if ( field[0][0] == CROSS && field[1][1] == CROSS && field[2][2] == CROSS) {
+        endGame = true;
+        alert("Победили крестики!");
+        for (let i = 0; i < 3; i++) {
+            let targetCell = findCell(i, i);
+            targetCell.style.color = "red";
+        }
+    } else if (field[0][2] == CROSS && field[1][1] == CROSS && field[2][0] == CROSS) {
+        endGame = true;
+        alert("Победили крестики!");
+        for (let i = 0; i < 3; i++) {
+            for (let j = 2; j >= 0; j--) {
+                let targetCell = findCell(i, 2 - i);
+                targetCell.style.color = "red";
+            }
+        }
+    } else if (field[0][0] == ZERO && field[1][1] == ZERO && field[2][2] == ZERO) {
+        endGame = true;
+        alert("Победили нолики!");
+        for (let i = 0; i < 3; i++) {
+            let targetCell = findCell(i, i);
+            targetCell.style.color = "red";
+        }
+    } else if (field[0][2] == ZERO && field[1][1] == ZERO && field[2][0] == ZERO) {
+        endGame = true;
+        alert("Победили нолики!");
+        for (let i = 0; i < 3; i++) {
+            let targetCell = findCell(i, 2 - i);
+            targetCell.style.color = "red";
+        }
+    }
 }
 
 function clickOnCell (row, col) {
