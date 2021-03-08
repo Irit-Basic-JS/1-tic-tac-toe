a68638638dee7e19188c186d841d45a606ef607a
alexxykv <alexxonov@gmail.com>
2021-03-09 00:51:12 +0500

    Задачи 1-8

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
 
+const RED = '#FF0000';
+
 const container = document.getElementById('fieldWrapper');
+
+let field;
+let gameStatus;
+let emptyCells;
+let currentSymbol;;
 
 startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    const dimension = 3
+
+    renderGrid(dimension);
+    createField(dimension);
+    emptyCells = dimension ** 2;
+    gameStatus = 'game';
+    currentSymbol = CROSS;
 }
 
 function renderGrid (dimension) {
@@ -26,14 +39,134 @@ function renderGrid (dimension) {
     }
 }
 
+function createField(dimension) {
+    field = [];
+    for (let i = 0; i < dimension; i++) {
+        field[i] = [];
+        for (let j = 0; j < dimension; j++){
+            field[i][j] = EMPTY;
+        }
+    }
+}
+
 function cellClickHandler (row, col) {
-    // Пиши код тут
+    if (gameStatus === 'game') {
+        if (field[row][col] === EMPTY) {
+            field[row][col] = currentSymbol;
+            renderSymbolInCell(currentSymbol, row, col);
+            currentSymbol = currentSymbol === CROSS ? ZERO : CROSS;
+            emptyCells--;
+        }
+
+        gameStatus = GameStatus();
+    }
+
     console.log(`Clicked on cell: ${row}, ${col}`);
+}
 
+function GameStatus () {
+    const message = (str) => setTimeout( () => alert(str), 50 );
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    let winner = hasWinner();
+    switch (winner) {
+        case CROSS:
+            message(`Победа ${CROSS}`);
+            return 'winner X';
+        case ZERO:
+            message(`Победа ${ZERO}`);
+            return 'winner O';
+        case 'draw':
+            message('Победила дружба');
+            return 'draw';
+        default: return 'game';
+    }
+}
+
+function hasWinner () {
+    const lines = getLines();
+
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i].symbols.includes(EMPTY)) continue;
+
+        let encounteredSymbols = new Set(lines[i].symbols);
+        if (encounteredSymbols.size === 1) {
+            highlightWinner(lines[i]);
+
+            if (lines[i].symbols[0] === CROSS) {
+                return CROSS;
+            } else return ZERO;
+        }
+    }
+
+    if (emptyCells === 0) return 'draw';
+}
+
+function getLines () {
+    const rows = getRows();
+    const columns = getColumns();
+    const diagonals = getDiagonals();
+
+    let lines = [];
+    lines.push(...rows);
+    lines.push(...columns);
+    lines.push(...diagonals);
+
+    return lines;
+}
+
+function getRows () {
+    let rows = [];
+
+    for (let i = 0; i < field.length; i++) {
+        row = {symbols: [], cells: []}
+        for (let j = 0; j < field.length; j++) {
+            row.symbols.push(field[i][j]);
+            row.cells.push([i, j]);
+        }
+        rows.push(row);
+    }
+
+    return rows;
+}
+
+function getColumns () {
+    let columns = [];
+
+    for (let i = 0; i < field.length; i++) {
+        let column = {symbols: [], cells: []};
+        for (let j = 0; j < field.length; j++) {
+            column.symbols.push(field[j][i]);
+            column.cells.push([j, i]);
+        }
+
+        columns.push(column);
+    }
+
+    return columns;
+}
+
+function getDiagonals () {
+    let diagonals = [ {symbols: [], cells: []}, {symbols: [], cells: []} ];
+
+    for (let i = 0; i < field.length; i++) {
+        diagonals[0].symbols.push(field[i][i]);
+        diagonals[0].cells.push([i, i]);
+
+        diagonals[1].symbols.push(field[field.length - 1 - i][i])
+        diagonals[1].cells.push([field.length - 1 - i, i]);
+    }
+
+    return diagonals;
+}
+
+function highlightWinner (line) {
+    const symbol = line.symbols[0];
+
+    for (let i = 0; i < line.cells.length; i++) {
+        let row = line.cells[i][0];
+        let col = line.cells[i][1];
+        renderSymbolInCell(symbol, row, col, RED);
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -54,6 +187,7 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
+    startGame();
     console.log('reset!');
 }
 
