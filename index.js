b151d2fcae44f3a82a6af4ec5ca981a7913005f2
philya71828 <philya71828@yandex.ru>
2021-08-07 22:29:25 +0500

    Task done 1

--- index.js
+++ index.js
@@ -4,19 +4,37 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+let sym; // Символ текущего хода
+let count = 0; // Количество совершенных ходов
+let dim = 3; // Размерность поля
+let arr = new Array(); // Массив, хранящий информацию о состоянии игрового поля
+
 startGame();
 addResetListener();
 
-function startGame () {
-    renderGrid(3);
+function startGame ()
+{
+    dim = Number(prompt('Размер поля:', dim)) || dim; // Запрос размера поля
+    sym = CROSS;
+    count = 0;
+    // Игровое поле пустое, соответственно заполняем массив 
+    for (let row = 0; row < dim; row++)
+    {
+        arr[row] = new Array(dim);
+        for (let col = 0; col < dim; col++)
+            arr[row][col] = EMPTY;
+    }
+    renderGrid(dim);
 }
 
-function renderGrid (dimension) {
+function renderGrid (dimension)
+{
     container.innerHTML = '';
-
-    for (let i = 0; i < dimension; i++) {
+    for (let i = 0; i < dimension; i++)
+    {
         const row = document.createElement('tr');
-        for (let j = 0; j < dimension; j++) {
+        for (let j = 0; j < dimension; j++)
+        {
             const cell = document.createElement('td');
             cell.textContent = EMPTY;
             cell.addEventListener('click', () => cellClickHandler(i, j));
@@ -26,41 +44,119 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
+function cellClickHandler (row, col)
+{
+    // Если ячейка не пустая, то прерываем выполнение функции
+    if ((findCell(row, col).textContent !== EMPTY) || (count == -1)) return;
+    count++;
+    renderSymbolInCell(sym, row, col); // Ставим символ
+    if (findWin(sym, row, col))
+    {
+        if (sym === CROSS)
+           setTimeout(() => alert('Победили крестики!'), 500);
+        else setTimeout(() => alert('Победили нолики!'), 500);
+        count = -1;
+        return;
+    }
+    if (count === dim * dim)
+    {
+        setTimeout(() => alert('Победила дружба!'), 500);
+        return;
+    }
+    // Меняем символ для следующего хода
+    sym = (sym === ZERO) ? CROSS : ZERO;
     console.log(`Clicked on cell: ${row}, ${col}`);
-
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+// Определяем, есть ли победа после текущего хода
+function findWin (symbol, row, col)
+{
+    // Проверяем сроку
+    for (let i = 0; i < dim; i++)
+    {
+        if (arr[row][i] !== symbol)
+            break;
+        // В случае победы, перекрашиваем символы
+        if (i == dim - 1)
+        {
+            for (i = 0; i < dim; i++)
+                renderSymbolInCell(symbol, row, i, '#FF0000');
+            return true;
+        }
+    }
+    // Проверяем столбец
+    for (let i = 0; i < dim; i++)
+    {
+        if (arr[i][col] !== symbol)
+            break;
+        if (i == dim - 1)
+        {
+            for (i = 0; i < dim; i++)
+                renderSymbolInCell(symbol, i, col, '#FF0000');
+            return true;
+        }
+    }
+    // Проверяем главную диагональ
+    if (row == col)
+        for (let i = 0; i < dim; i++)
+        {
+            if(arr[i][i] !== symbol)
+                break;
+            if (i == dim - 1)
+            {
+                for (i = 0; i < dim; i++)
+                    renderSymbolInCell(symbol, i, i, '#FF0000');
+                return true;
+            }
+        }
+    // Проверяем побочную диагональ
+    if (row == dim - col - 1)
+        for (let i = 0; i < dim; i++)
+        {
+            if(arr[i][dim - i - 1] !== symbol)
+                break;
+            if (i == dim - 1)
+            {
+                for (i = 0; i < dim; i++)
+                    renderSymbolInCell(symbol, i, dim - i - 1, '#FF0000');
+                return true;
+            }
+        }
+}
+
+function renderSymbolInCell (symbol, row, col, color = '#333')
+{
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
+    arr[row][col] = sym;
 }
 
-function findCell (row, col) {
+function findCell (row, col)
+{
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener ()
+{
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+//Нажатие кнопки "Сначала"
+function resetClickHandler ()
+{
+    startGame();
     console.log('reset!');
 }
 
 
 /* Test Function */
 /* Победа первого игрока */
-function testWin () {
+function testWin ()
+{
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -71,7 +167,8 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw ()
+{
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -84,6 +181,7 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell (row, col)
+{
     findCell(row, col).click();
 }
