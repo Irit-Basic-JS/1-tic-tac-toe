fda2aa9bea28693586e4498a8eac243a9f872ebc
Tripletri <Tripletri@yandex.ru>
2021-09-28 13:20:47 +0500

    1-10
    - Пункты 1-10
    - Сменил на темную тему

--- index.js
+++ index.js
@@ -2,88 +2,311 @@ const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
 
-const container = document.getElementById('fieldWrapper');
+class Point {
+    x;
+    y;
 
-startGame();
-addResetListener();
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 
-function startGame () {
-    renderGrid(3);
-}
+    toString() {
+        return (`${(this.x)}, ${this.y}`);
+    }
 
-function renderGrid (dimension) {
-    container.innerHTML = '';
-
-    for (let i = 0; i < dimension; i++) {
-        const row = document.createElement('tr');
-        for (let j = 0; j < dimension; j++) {
-            const cell = document.createElement('td');
-            cell.textContent = EMPTY;
-            cell.addEventListener('click', () => cellClickHandler(i, j));
-            row.appendChild(cell);
-        }
-        container.appendChild(row);
+    hashCode() {
+        return this.x * 509 + this.y;
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
+const rowSelector = (index, i) => {
+    return [index, i];
+};
+const columnSelector = (index, i) => {
+    return [i, index];
+};
+const upDownDiagonalSelector = (index, i) => {
+    return [i, i];
+};
+
+const downUpDiagonalSelector = (index, i, dimension) => {
+    return [dimension - i - 1, i];
+};
+
+const createDownUpDiagonalSelector = dimension => {
+    return (index, i) => downUpDiagonalSelector(index, i, dimension);
+};
 
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+class Controller {
+    useBotInput = document.getElementById('useBot');
+    dimensionInput = document.getElementById('dimension');
+    resetButton = document.getElementById('reset');
+
+    onCellClick;
+    onReset;
+
+    constructor() {
+        this.resetButton.addEventListener('click', () => this.onResetHandler());
+    }
+
+    getDimension() {
+        return this.dimensionInput.value;
+    }
+
+    setDimension(value) {
+        this.dimensionInput.value = value;
+    }
+
+    cellClickHandler(i, j) {
+        this.onCellClick(i, j);
+    }
+
+    onResetHandler() {
+        this.onReset();
+    }
+
+    isUseBot() {
+        return this.useBotInput.checked;
+    }
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
-    const targetCell = findCell(row, col);
+class Game {
+    MAX_DIMENSION = 100;
 
-    targetCell.textContent = symbol;
-    targetCell.style.color = color;
+    grid = [];
+    freeCells = new Map();
+    isGameEnd = false;
+    dimension = 3;
+    turn = CROSS;
+    validators = [];
+
+    controller;
+    view;
+
+    constructor(controller, view) {
+        this.controller = controller;
+        this.view = view;
+        controller.onCellClick = (i, j) => this.handleCellClick(i, j);
+        controller.onReset = () => this.startGame();
+
+        this.validators = [
+            new IterWinnerChecker(this, rowSelector),
+            new IterWinnerChecker(this, columnSelector),
+            new StaticWinnerChecker(this, upDownDiagonalSelector),
+            new StaticWinnerChecker(this, createDownUpDiagonalSelector(this.dimension))
+        ];
+    }
+
+    startGame() {
+        this.setDimension();
+        this.view.renderGrid(this.dimension);
+        this.initializeGrid();
+        this.isGameEnd = false;
+        this.turn = CROSS;
+    }
+
+    setDimension() {
+        this.dimension = this.controller.getDimension();
+        if (this.dimension > this.MAX_DIMENSION) {
+            this.controller.setDimension(this.MAX_DIMENSION);
+            this.dimension = this.MAX_DIMENSION;
+        }
+    }
+
+    initializeGrid() {
+        this.freeCells.clear();
+        this.fillFreeCells();
+    }
+
+    fillFreeCells() {
+        for (let i = 0; i < this.dimension; i++) {
+            this.grid[i] = [];
+            for (let j = 0; j < this.dimension; j++) {
+                this.grid[i][j] = EMPTY;
+                const point = new Point(i, j);
+                this.freeCells.set(point.hashCode(), point);
+            }
+        }
+    }
+
+    handleCellClick(row, col) {
+        if (!this.canMakeTurn(row, col))
+            return;
+        this.makeTurn(row, col);
+        if (this.controller.isUseBot())
+            this.makeBotTurn();
+    }
+
+    makeTurn(row, col) {
+        this.setTurn(row, col);
+        this.checkWinner();
+        this.checkDraw();
+        this.swapTurn();
+    }
+
+    canMakeTurn(row, col) {
+        return this.grid[row][col] === EMPTY && !this.isGameEnd;
+    }
+
+    setTurn(row, col) {
+        this.grid[row][col] = this.turn;
+        this.freeCells.delete(new Point(row, col).hashCode());
+        view.renderSymbolInCell(this.turn, row, col);
+    }
+
+    checkWinner() {
+        for (let i in this.validators) {
+            const winner = this.validators[i].check();
+            if (winner) {
+                this.announceWinner(...winner);
+                break;
+            }
+        }
+    }
+
+
+    checkDraw() {
+        if (this.freeCells.size === 0 && !this.isGameEnd) {
+            view.alert("Победила дружба");
+            this.isGameEnd = true;
+        }
+    }
+
+    swapTurn() {
+        if (this.turn === ZERO)
+            this.turn = CROSS;
+        else if (this.turn === CROSS)
+            this.turn = ZERO;
+    }
+
+    makeBotTurn() {
+        const turnKeys = Array.from(this.freeCells.keys());
+        if (turnKeys.length === 0)
+            return;
+        const turnKey = turnKeys[getRandomInt(turnKeys.length)];
+        const turn = this.freeCells.get(turnKey);
+        this.makeTurn(turn.x, turn.y);
+    }
+
+    announceWinner(index, selector) {
+        this.view.paintLine(index, selector, this.turn);
+        this.isGameEnd = true;
+        this.view.alert(`Победил ${this.turn}`);
+    }
 }
 
-function findCell (row, col) {
-    const targetRow = container.querySelectorAll('tr')[row];
-    return targetRow.querySelectorAll('td')[col];
+class WinnerChecker {
+    game;
+
+    constructor(game) {
+        this.game = game;
+    }
+
+    isLineWon(index, selector) {
+        let firstTurn;
+        for (let i = 0; i < this.game.dimension; i++) {
+            const [row, col] = selector(index, i);
+            let cell = this.game.grid[row][col];
+            if (cell === EMPTY)
+                return false;
+            if (firstTurn === undefined)
+                firstTurn = cell;
+            if (firstTurn !== cell)
+                return false;
+        }
+        return true;
+    }
+
+    check() {
+    }
 }
 
-function addResetListener () {
-    const resetButton = document.getElementById('reset');
-    resetButton.addEventListener('click', resetClickHandler);
+class IterWinnerChecker extends WinnerChecker {
+    selector;
+
+    constructor(game, selector) {
+        super(game);
+        this.selector = selector;
+    }
+
+    check() {
+        for (let i = 0; i < this.game.grid.length; i++) {
+            if (this.isLineWon(i, this.selector)) {
+                return [i, this.selector];
+            }
+        }
+    }
 }
 
-function resetClickHandler () {
-    console.log('reset!');
+class StaticWinnerChecker extends WinnerChecker {
+    constructor(game, selector) {
+        super(game);
+        this.selector = selector;
+    }
+
+    check() {
+        if (this.isLineWon(0, this.selector))
+            return [0, this.selector];
+    }
 }
 
 
-/* Test Function */
-/* Победа первого игрока */
-function testWin () {
-    clickOnCell(0, 2);
-    clickOnCell(0, 0);
-    clickOnCell(2, 0);
-    clickOnCell(1, 1);
-    clickOnCell(2, 2);
-    clickOnCell(1, 2);
-    clickOnCell(2, 1);
+class View {
+    container = document.getElementById('fieldWrapper');
+
+    controller;
+
+    constructor(controller) {
+        this.controller = controller;
+    }
+
+    renderGrid() {
+        this.container.innerHTML = '';
+
+        for (let i = 0; i < this.controller.getDimension(); i++) {
+            const row = document.createElement('tr');
+            for (let j = 0; j < this.controller.getDimension(); j++) {
+                const cell = document.createElement('td');
+                cell.textContent = EMPTY;
+                cell.addEventListener('click', () => controller.cellClickHandler(i, j));
+                row.appendChild(cell);
+            }
+            this.container.appendChild(row);
+        }
+    }
+
+    renderSymbolInCell(symbol, row, col, color = '#333') {
+        const targetCell = this.findCell(row, col);
+
+        targetCell.textContent = symbol;
+        targetCell.style.color = color;
+    }
+
+    findCell(row, col) {
+        const targetRow = this.container.querySelectorAll('tr')[row];
+        return targetRow.querySelectorAll('td')[col];
+    }
+
+    paintLine(index, selector, symbol) {
+        for (let i = 0; i < this.controller.getDimension(); i++) {
+            let [row, col] = selector(index, i);
+            this.renderSymbolInCell(symbol, row, col, '#ff0000');
+        }
+    }
+
+    alert(msg) {
+        alert(msg);
+    }
 }
 
-/* Ничья */
-function testDraw () {
-    clickOnCell(2, 0);
-    clickOnCell(1, 0);
-    clickOnCell(1, 1);
-    clickOnCell(0, 0);
-    clickOnCell(1, 2);
-    clickOnCell(1, 2);
-    clickOnCell(0, 2);
-    clickOnCell(0, 1);
-    clickOnCell(2, 1);
-    clickOnCell(2, 2);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
-function clickOnCell (row, col) {
-    findCell(row, col).click();
-}
+const controller = new Controller();
+const view = new View(controller);
+const game = new Game(controller, view);
+game.startGame();
