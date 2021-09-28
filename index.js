63b1adf1cf77ff044cadf04e113b6b52c268f75b
debebantur <stepan.filippov@yandex.ru>
2021-09-28 22:33:12 +0500

    Филиппов Степан Владимирович
    решил до 11

--- index.js
+++ index.js
@@ -4,20 +4,30 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+let tableSize;
+let table ;
+let totalPlaced;
+let inGame = false;
 startGame();
 addResetListener();
 
-function startGame () {
-    renderGrid(3);
+function startGame () { // обновление поля
+    inGame = true;
+    tableSize = prompt('Задайте сторону поля',3);
+    table = new Array(tableSize);
+    totalPlaced = 0;
+    renderGrid(tableSize);
 }
 
-function renderGrid (dimension) {
+function renderGrid (dimension) {//создаёт поле
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
         const row = document.createElement('tr');
+        table[i] = new Array(tableSize);
         for (let j = 0; j < dimension; j++) {
             const cell = document.createElement('td');
+            table[i][j] = EMPTY;
             cell.textContent = EMPTY;
             cell.addEventListener('click', () => cellClickHandler(i, j));
             row.appendChild(cell);
@@ -26,35 +36,138 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
-
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+function cellClickHandler (row, col) {   // срабатывает по клику на поле
+    if(!inGame)
+        return;
+    let cell = findCell(row, col);
+    if(cell.textContent == EMPTY){
+        placeSymbolInCell(CROSS, row, col);
+        console.log(`Clicked on cell: ${row}, ${col}`);
+        //console.log('stupidIntelligence start');
+        if(inGame)
+            stupidIntelligence();
+        //console.log('stupidIntelligence end');
+    }
+    else
+        console.log(`Clicked on not empty cell: ${row}, ${col}`);
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
-    const targetCell = findCell(row, col);
+function placeSymbolInCell (symbol, row, col) { //записывает символ
+    drawSymbol (symbol, row, col, color = '#333')
+    table[row][col] = symbol;
+    totalPlaced+=1;
+    findReasonToEndGame(row, col);
+}
 
+function drawSymbol (symbol, row, col, color) {
+    const targetCell = findCell(row, col);
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell (row, col) {// возвращает клетку
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener () { //привязка кнопки перезапуска
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+function resetClickHandler () { // работа кнопки перезапуска
+    startGame();
     console.log('reset!');
+}
+
+function stupidIntelligence(){ //интеллект из пункта 10
+    while(true){
+        let row = Math.floor((Math.random()*tableSize))%tableSize;
+        let col = Math.floor((Math.random()*tableSize))%tableSize;
+        let cell = findCell(row, col)
+        if (cell.textContent == EMPTY){
+            placeSymbolInCell(ZERO, row, col);
+            break;
+        }
+    }
+}
+
+function findReasonToEndGame(lastRow, lastCol){ // обработка возможных окончаний игры
+    if(totalPlaced == tableSize*tableSize){
+        inGame = false;
+        alert('победила дружба');
+        return;
+    }
+
+    let sym = findCell(lastRow, lastCol).textContent;
+    let win = true;
+
+    for(let i =0; i<tableSize; i++){
+        if(table[lastRow][i]!=sym){
+            win = false;
+            break;
+        }
+    }
+    if(win){
+        for(let i =0; i<tableSize; i++)
+            drawSymbol (sym, lastRow, i, color = '#f50000')
+        alert(`победила команда ${sym}`);
+        inGame = false;
+        console.log('победа по вертикали');
+        return;
+    }
+
+    win = true;
+    for(let i =0; i<tableSize; i++){
+        if(table[i][lastCol]!=sym){
+            win = false;
+            break;
+        }
+    }
+    if(win){
+        for(let i =0; i<tableSize; i++)
+            drawSymbol (sym, i, lastCol, color = '#f50000')
+        alert(`победила команда ${sym}`);
+        inGame = false;
+        console.log('победа по горизонтали');
+        return;
+    }
+
+    if(lastCol == lastRow){
+        win = true;
+        for(let i =0; i<tableSize; i++){
+            if(table[i][i]!=sym){
+                 win = false;
+                break;
+            }
+        }
+        if(win){
+            for(let i =0; i<tableSize; i++)
+            drawSymbol (sym, i, i, color = '#f50000')
+            alert(`победила команда ${sym}`);
+            inGame = false;
+            console.log('победа \\');
+            return;
+        }
+    }
+
+    if((tableSize -1-lastCol) == lastRow){
+        win = true;
+        for(let i =0; i<tableSize; i++){
+            if(table[i][tableSize -1-i]!=sym){
+                 win = false;
+                break;
+            }
+        }
+        if(win){
+            for(let i =0; i<tableSize; i++)
+            drawSymbol (sym, i, tableSize -1-i, color = '#f50000')
+            alert(`победила команда ${sym}`);
+            inGame = false;
+            console.log('победа /');
+            return;
+        }
+    }
 }
 
 
