eaefb9002ddf570a00c12866e42c8a83aed8112e
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2021-10-01 00:45:54 +0500

    1-10 пунктов

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+let fieldArr = [];
+let gameOver = false;
+let sizeField = 3;
+
+
+let inputSize = document.querySelector('.input-size');
+let buttonSize = document.querySelector('.input-button');
+
+buttonSize.addEventListener('click', () => {
+    sizeField = inputSize.value;
+    startGame();
+    addResetListener();
+    resetClickHandler();
+})
+
 startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    renderGrid(sizeField);
 }
 
 function renderGrid (dimension) {
@@ -24,16 +39,158 @@ function renderGrid (dimension) {
         }
         container.appendChild(row);
     }
+    
+    createDataArr();
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
+function createDataArr() {
+    fieldArr = [];
+    let rows = container.getElementsByTagName('tr');
+    for(let i = 0; i < rows.length; i++) {
+        let col = rows[i].getElementsByTagName('td');
+        let temporary = [];
+        for(let j = 0; j < rows.length; j++) {
+            temporary.push(col[j]);
+        }
+        fieldArr.push(temporary);
+    }
+}
 
+let lastMove = EMPTY;
+let move = 1;
 
-    /* Пользоваться методом для размещения символа в клетке так:
+function cellClickHandler(row, col) {
+    if(gameOver || findCell(row, col).textContent != EMPTY) 
+        return;
+
+    console.log(`Clicked on cell: ${row}, ${col}`);    
+
+    if(lastMove == CROSS) {
         renderSymbolInCell(ZERO, row, col);
-     */
+        fieldArr[row][col].textContent = ZERO;
+    } else {
+        renderSymbolInCell(CROSS, row, col);
+        fieldArr[row][col].textContent = CROSS;
+    }
+    lastMove = lastMove == CROSS ? ZERO : CROSS;
+
+    findWinner();
+    if(!gameOver){
+        checkMove();
+    }
+
+    if(lastMove == CROSS) {        
+        setTimeout(() => {
+            botRandom();    
+        }, 30);        
+    }
+}
+
+function botRandom() {
+
+    let interval = setInterval(() => {
+        let row = Math.floor(Math.random() * sizeField);
+        let col = Math.floor(Math.random() * sizeField);
+
+        if(findCell(row, col).textContent == EMPTY) {
+            cellClickHandler(row, col);
+            clearInterval(interval);
+        }
+    }, 10);
+}
+
+function findWinner() {
+    let winner = "";
+
+    let resRows = '';
+    let resColumns = '';
+    let resDiagonalRigth = '';
+    let resDiagonalLeft = '';
+    let resDataRows = [];
+    let resDataColumns = [];
+    let resDataDiagonalRigth = [];
+    let resDataDiagonalLeft = [];
+
+    for(let i = 0; i < fieldArr.length; i++) {
+        resRows = '';
+        resColumns = '';
+        resDataRows = [];
+        resDataColumns = [];
+
+        resDiagonalRigth += fieldArr[i][i].textContent;
+        resDataDiagonalRigth.push(fieldArr[i][i]);
+
+        resDiagonalLeft += fieldArr[fieldArr.length - 1 - i][i].textContent;
+        resDataDiagonalLeft.push(fieldArr[fieldArr.length - 1 - i][i]);
+
+        for(let j = 0; j < fieldArr.length; j++) {
+            resRows += fieldArr[i][j].textContent;
+            resDataRows.push(fieldArr[i][j]);
+
+            resColumns += fieldArr[j][i].textContent;
+            resDataColumns.push(fieldArr[j][i]);
+        }
+
+        winner = chooseWinner(resRows);
+        if(winner == '') {
+            winner = chooseWinner(resColumns);
+            if(winner != '') {
+                writeWinner(winner, resDataColumns);
+                return;
+            } else {
+                winner = chooseWinner(resDiagonalRigth);
+                if(winner != '') {
+                    writeWinner(winner, resDataDiagonalRigth);
+                    return;
+                } else {
+                    winner = chooseWinner(resDiagonalLeft);
+                    if(winner != '') {
+                        writeWinner(winner, resDataDiagonalLeft);
+                        return;
+                    }
+                }
+            }
+        } else {
+            writeWinner(winner, resDataRows);
+            return;
+        }
+    }
+}
+
+function writeWinner(winner, resData) {
+    for(let e of resData) {
+        e.style.color = '#f00';
+    }
+    setTimeout(() => {
+        alert(`Победил ${winner}`);    
+    }, 10);
+    gameOver = true;
+}
+
+function chooseWinner(name) {
+    let winner = '';
+    if(name == CROSS.repeat(sizeField)) {
+        winner = CROSS;            
+    }
+    else if(name == ZERO.repeat(sizeField)) {
+        winner = ZERO;
+    }
+    return winner;
+}
+
+function checkMove() {
+    for(let i = 0; i < fieldArr.length; i++) {
+        for(let j = 0; j < fieldArr.length; j++) {
+            if(fieldArr[i][j].textContent == EMPTY) {
+                return;
+            }
+        }
+    }
+
+    setTimeout(() => {
+        alert('Победила дружба');
+        gameOver = true;   
+    }, 10);
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -54,6 +211,13 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
+    let rows = container.querySelectorAll('tr');
+    for(let elementRow of rows)
+        for(let cell of elementRow.getElementsByTagName('td')){
+            cell.textContent = EMPTY;
+        }
+    gameOver = false;
+    lastMove = EMPTY;
     console.log('reset!');
 }
 
