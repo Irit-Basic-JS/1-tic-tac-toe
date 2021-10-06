5a130ab432219d5adc2e59683ac6d63963dd3a49
Mikhail Lanskikh <lanskikh-mikhail@mail.ru>
2021-10-06 20:19:15 +0500

    commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
+const grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+console.log(grid);
 
 const container = document.getElementById('fieldWrapper');
 
@@ -27,20 +29,19 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
-
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    if (!isGameFinished() && grid[row][col] === ' ') {
+        if (!isTheMostCrosses()) renderSymbolInCell(CROSS, row, col);
+        else renderSymbolInCell(ZERO, row, col);
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
-
+    grid[row][col] = symbol;
     targetCell.textContent = symbol;
     targetCell.style.color = color;
+    sayWinner();
+    sayDraw();
 }
 
 function findCell (row, col) {
@@ -57,6 +58,53 @@ function resetClickHandler () {
     console.log('reset!');
 }
 
+function isTheMostCrosses() {
+    const symbols = grid.flat();
+    const crossFilter = element => element === CROSS;
+    const zeroFilter = element => element === ZERO;
+    return symbols.filter(crossFilter).length > symbols.filter(zeroFilter).length;
+}
+
+function isGameFinished() {
+    return hasWinner() || !grid.flat().filter(element => element === EMPTY).length;
+}
+
+function hasWinner() {
+    return whoIsWinner()
+}
+
+function whoIsWinner() {
+    const verticalChecker = []
+    const diagonalChecker1 = []
+    const diagonalChecker2 = []
+    for(let i = 0; i < grid.length; i++) {
+        if (grid[i].filter(item => item === CROSS).length === grid.length) {
+            return CROSS;
+        } else if (grid[i].filter(item => item === ZERO).length === grid.length) {
+            return ZERO;
+        }
+        diagonalChecker1.push(grid[i][i]);
+        diagonalChecker2.push(grid[grid.length - 1 - i][grid.length - 1 - i]);
+        for(let j = 0; j < grid.length; j++) {
+            verticalChecker.push(grid[j][i]);
+        }
+        if (verticalChecker.filter(item => item === CROSS).length === grid.length) return CROSS;
+        else if (verticalChecker.fill(item => item === ZERO).length === grid.length) return ZERO;
+        verticalChecker = []
+    }
+    if (diagonalChecker1.filter(item => item === CROSS).length === grid.length) return CROSS;
+    else if (diagonalChecker1.filter(item => item === CROSS).length === grid.length) return ZERO;
+    if (diagonalChecker2.filter(item => item === CROSS).length === grid.length) return CROSS;
+    else if (diagonalChecker2.filter(item => item === ZERO).length === grid.length) return ZERO;
+}
+
+function sayDraw() {
+    if (isGameFinished()) alert('Победила дружба');
+}
+
+function sayWinner() {
+    if(hasWinner()) alert(whoIsWinner());
+}
 
 /* Test Function */
 /* Победа первого игрока */
