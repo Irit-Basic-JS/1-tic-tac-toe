b30ff79ee7700c0055e338ec0a5b2ef4aa0d2109
Honk <lennoninmyheart@gmail.com>
2021-10-08 00:20:22 +0500

    Назарова Анна
    1-12 финалочка.
    p.s. при расширении поля умный ИИ не имеет смысла бтв.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
+const size = +prompt("Размер поля")
 let gField = new gameField(3)
 
 const container = document.getElementById('fieldWrapper');
@@ -9,37 +10,49 @@ startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    renderGrid(size);
 }
 
 function gameField (size) {
     this.count = 0;
     this.size = size;
     this.winner = null;
+    this.getWinCombination = [];
     this.grid = [];
-    this.fillGrid = function () {
-        for (let i = 0; i < this.size; i++) {
-            this.grid.push([]);
-            for (let j = 0; j < this.size; j++) {
-                this.grid[i].push(null);
-            }
+    this.artIntSteps = [];
+    for (let i = 0; i < this.size; i++) {
+        this.grid.push([]);
+        for (let j = 0; j < this.size; j++) {
+            this.grid[i][j] = null;
         }
     }
-    this.fillGrid();
 
     this.add = function (symbol, row, col) {
         if (!(this.grid[row][col] === null)) return;
         this.grid[row][col] = symbol;
-        this.updateWinner(row, col);
         if (this.gameOver()) alert(this.winner);
         this.count++;
+        this.updateWinner(row, col);
     }
 
     this.gameOver = function () {
         return (this.winner || this.count === (this.size * this.size))
     }
 
+    this.gridExpansion = function () {
+        this.grid.push([]);
+        for (let i = 0; i < this.grid.length - 1; i++) {
+            this.grid[this.grid.length - 1].push(null)
+        }
+
+        for (let j = 0; j < this.grid.length; j++) {
+            this.grid[j].push(null)
+        }
+    }
+
     this.updateWinner = function (row, col) {
+        if (this.count < this.size * 2 - 1)
+            return;
         let counters = [0, 0, 0, 0];
         for (let i = 0; i < this.size; i++) {
             const coordinates = [this.grid[i][col], this.grid[row][i],
@@ -51,6 +64,7 @@ function gameField (size) {
             }
         }
 
+        console.log(counters);
         if (counters.includes(this.size)) {
             this.winner = CROSS;
         }
@@ -58,65 +72,91 @@ function gameField (size) {
         if (counters.includes(-this.size)) {
             this.winner = ZERO;
         }
+
+        if (this.winner) {
+            let winType = counters.indexOf(this.winner === CROSS ? this.size : -this.size);
+            this.updateWinCombination(winType, row, col);
+        }
     }
 
-    this.getWinCombination = function () {
-        let res = [];
-        if (this.grid[0][0] === this.grid[this.size - 1][this.size - 1] &&
-            this.grid[0][0] === this.winner) {
-            for (let i = 0; i < this.size; i++) {
-                res.push([i, i]);
-            }
-        }
-        if (this.grid[0][this.size - 1] === this.grid[this.size - 1][0] &&
-            this.grid[0][this.size - 1] === this.winner) {
-            for (let i = 0; i < this.size; i++) {
-                res.push([i, this.size - 1 - i]);
-            }
-        }
+    this.updateWinCombination = function (winType, row, col) {
         for (let i = 0; i < this.size; i++) {
-            if (this.grid[0][i] === this.grid[this.size - 1][i] && this.grid[0][i] === this.winner) {
-                for (let j = 0; j < this.size; j++) {
-                    res.push([j, i]);
+            if (winType === 0) this.getWinCombination.push([i, col]);
+            if (winType === 1) this.getWinCombination.push([row, i]);
+            if (winType === 2) this.getWinCombination.push([i, i]);
+            if (winType === 3) this.getWinCombination.push([i, this.size - 1 - i]);
+        }
+    }
+}
+
+function getEmptyCell(field) {
+    let maxNumber = field.size * field.size - field.count;
+    return Math.floor(Math.random() * (maxNumber + 1));
+}
+
+function winColor() {
+    for (const pair of gField.getWinCombination) {
+        renderSymbolInCell(gField.winner, pair[0], pair[1], '#FF0000');
+    }
+}
+
+function gameResult() {
+    if (gField.winner === ZERO) {
+        alert("Победили нолики");
+    } else if (gField.winner === CROSS) {
+        alert("Победили крестики");
+    } else {
+        alert("Победила дружба");
+    }
+}
+
+function moveOnCell(symbol, row, col) {
+    if (gField.grid[row][col] === null) {
+        gField.add(symbol, row, col);
+        renderSymbolInCell(symbol, row, col);
+    }
+    if (gField.gameOver()) {
+        winColor();
+        gameResult();
+    } else if (gField.count * 2 > gField.size ** 2) {
+        gField.size += 1;
+        gField.gridExpansion();
+        renderGrid(gField.size);
+        for (let i = 0; i < gField.grid.length - 1; i++) {
+            for (let j = 0; j < gField.grid.length; j++) {
+                renderSymbolInCell(gField.grid[i][j], i, j);
+            }
+        }
+    }
+    console.log(gField.grid);
+}
+
+function artIntSteps () {
+    let emptyCell = getEmptyCell(gField);
+    let count = 0;
+    for (let i = 0; i < gField.size; i++) {
+        for (let j = 0; j < gField.size; j++) {
+            if (gField.grid[i][j] === null) {
+                count += 1;
+                if (count === emptyCell) {
+                    moveOnCell(ZERO, i, j);
+                    gField.artIntSteps.push([i,j]);
+                    console.log('AI step on cell: ${i}, ${j}');
+                    return;
                 }
             }
         }
-        for (let i = 0; i < this.size; i++) {
-            if (this.grid[i][0] === this.grid[i][this.size - 1] && this.grid[i][0] === this.winner) {
-                for (let j = 0; j < this.size; j++) {
-                    res.push([i, j]);
-                }
-            }
-        }
-        return res;
     }
 }
 
 function cellClickHandler (row, col) {
-    console.log(gField.count)
-    const sym = (gField.count % 2 === 0) ? CROSS : ZERO;
-
-    if (!gField.gameOver()) {
-        gField.add(sym, row, col);
-        renderSymbolInCell(sym, row, col);
+    if(gField.count % 2 === 0) {
+        moveOnCell(CROSS, row, col);
+        console.log('U step on cell: ${row}, ${col}')
+    } else {
+        artIntSteps();
     }
-
-    if (gField.gameOver()) {
-        if (gField.winner === ZERO) {
-            alert("Победили нолики");
-        } else if (gField.winner === CROSS) {
-            alert("Победили крестики");
-        } else {
-            alert("Победила дружба");
-        }
-        let winCombination = gField.getWinCombination();
-        for (const pair of winCombination) {
-            renderSymbolInCell(gField.winner, pair[0], pair[1], '#FF0000');
-        }
-    }
-    console.log(`Clicked on cell: ${row}, ${col}`);
 }
-
 function renderGrid(dimension) {
     container.innerHTML = '';
 
@@ -149,32 +189,11 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
-    gField = new gameField(3);
+    gField = new gameField(size);
     for (let i = 0; i < gField.size; i++) {
         for (let j = 0; j < gField.size; j++) {
             renderSymbolInCell(EMPTY, i, j);
         }
     }
+    renderGrid(size);
 }
-
-/* Test Function
-function testWin () {
-    clickOnCell(2, 2);
-}
-
-function testDraw () {
-    clickOnCell(2, 0);
-    clickOnCell(1, 0);
-    clickOnCell(1, 1);
-    clickOnCell(0, 0);
-    clickOnCell(1, 2);
-    clickOnCell(1, 2);
-    clickOnCell(0, 2);
-    clickOnCell(0, 1);
-    clickOnCell(2, 1);
-    clickOnCell(2, 2);
-}
-
-function clickOnCell (row, col) {
-    findCell(row, col).click();
-}*/
